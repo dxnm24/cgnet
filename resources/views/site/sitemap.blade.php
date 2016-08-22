e563bcc27475ba64e0bb3cc09dee190765ad0fb4
dxnm <2711sq@gmail.com>
2016-08-22 15:56:46 +0700

    fix

--- resources/views/site/sitemap.blade.php
+++ resources/views/site/sitemap.blade.php
@@ -5,34 +5,45 @@
         <changefreq>always</changefreq>
         <priority>1</priority>
     </url>
-    @foreach(CommonQuery::getAllWithStatus('game_types') as $value)
     <?php 
-        if($value->parent_id > 0) {
-            $parentSlug = CommonQuery::getFieldById('game_types', $value->parent_id, 'slug');
-            $gameTypeUrl = CommonUrl::getUrl2($parentSlug, $value->slug);
-        } else {
-            $gameTypeUrl = CommonUrl::getUrl($value->slug);
-        }
+        $gameTypes = CommonQuery::getAllWithStatus('game_types');
+        $gameTags = CommonQuery::getAllWithStatus('game_tags');
+        $games = CommonQuery::getAllWithStatus('games');
     ?>
-    <url>
-    	<loc>{{ $gameTypeUrl }}</loc>
-		<changefreq>weekly</changefreq>
-		<priority>0.8</priority>
-    </url>
-    @endforeach
-    @foreach(CommonQuery::getAllWithStatus('game_tags') as $value)
-    <url>
-    	<loc>{{ CommonUrl::getUrlGameTag($value->slug) }}</loc>
-		<changefreq>weekly</changefreq>
-		<priority>0.8</priority>
-    </url>
-    @endforeach
-    @foreach(CommonQuery::getAllWithStatus('games') as $value)
-	    <url>
-	    	<loc>{{ CommonUrl::getUrl($value->slug) }}</loc>
-	    	<lastmod>{{ date('Y-m-d', strtotime($value->start_date)) }}</lastmod>
-			<changefreq>weekly</changefreq>
-			<priority>0.8</priority>
-	    </url>
-	@endforeach
+    @if($gameTypes)
+        @foreach($gameTypes as $value)
+        <?php 
+            if($value->parent_id > 0) {
+                $parentSlug = CommonQuery::getFieldById('game_types', $value->parent_id, 'slug');
+                $gameTypeUrl = CommonUrl::getUrl2($parentSlug, $value->slug);
+            } else {
+                $gameTypeUrl = CommonUrl::getUrl($value->slug);
+            }
+        ?>
+        <url>
+            <loc>{{ $gameTypeUrl }}</loc>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        @endforeach
+    @endif
+    @if($gameTags)
+        @foreach($gameTags as $value)
+        <url>
+            <loc>{{ CommonUrl::getUrlGameTag($value->slug) }}</loc>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        @endforeach
+    @endif
+    @if($games)
+        @foreach($games as $value)
+            <url>
+                <loc>{{ CommonUrl::getUrl($value->slug) }}</loc>
+                <lastmod>{{ date('Y-m-d', strtotime($value->start_date)) }}</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.8</priority>
+            </url>
+        @endforeach
+    @endif
 </urlset>
