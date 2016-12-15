2b978a8d1f26c2beb20fe40e45f6e7be3a8b18e9
nmd <you@example.com>
2016-12-15 15:21:20 +0700

    fix

--- resources/views/site/listgames.blade.php
+++ resources/views/site/listgames.blade.php
@@ -1,6 +1,26 @@
 <?php 
+	$gameTypes = CommonQuery::getAllWithStatus('game_types');
+    $gameTags = CommonQuery::getAllWithStatus('game_tags');
     $games = CommonQuery::getAllWithStatus('games');
 ?>
+@if($gameTypes)
+    @foreach($gameTypes as $value)
+    <?php 
+        if($value->parent_id > 0) {
+            $parentSlug = CommonQuery::getFieldById('game_types', $value->parent_id, 'slug');
+            $gameTypeUrl = url($parentSlug.'/'.$value->slug);
+        } else {
+            $gameTypeUrl = url($value->slug);
+        }
+    ?>
+    	{{ $gameTypeUrl }}<br>
+    @endforeach
+@endif
+@if($gameTags)
+    @foreach($gameTags as $value)
+    	{{ url('tag/'.$value->slug) }}<br>
+    @endforeach
+@endif
 @if($games)
     @foreach($games as $value)
        {{ url($value->slug) }}<br>
