055b98338fe6e8a224a4d3a645343c8b1da741fa
nmd <you@example.com>
2017-02-15 00:40:39 +0700

    fix

--- resources/views/site/common/ad.blade.php
+++ resources/views/site/common/ad.blade.php
@@ -1,9 +1,9 @@
 <?php 
 	$posPc = isset($posPc)?$posPc:null;
-	$postMobile = isset($postMobile)?$postMobile:null;
+	$posMobile = isset($posMobile)?$posMobile:null;
 ?>
 @if(getDevice() == PC)
 	{!! CommonQuery::getAdByPosition($posPc) !!}
 @else
-	{!! CommonQuery::getAdByPosition($postMobile) !!}
+	{!! CommonQuery::getAdByPosition($posMobile) !!}
 @endif
