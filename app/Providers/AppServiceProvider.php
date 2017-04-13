cb92ee1e113ebecf00be1fed2439afe0e2180754
nmd <you@example.com>
2017-04-13 18:04:46 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use DB;
+use Cache;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,20 +15,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $topmenu = DB::table('menus')
-            ->where('status', ACTIVE)
-            ->where('type', MENUTYPE1)
-            ->orderByRaw(DB::raw("position = '0', position"))
-            ->orderBy('name')
-            ->get();
-        $sidemenu = DB::table('menus')
-            ->where('status', ACTIVE)
-            ->where('type', MENUTYPE2)
-            ->orderByRaw(DB::raw("position = '0', position"))
-            ->orderBy('name')
-            ->get();
-        view()->share('topmenu', $topmenu);
-        view()->share('sidemenu', $sidemenu);
+        self::getMenus(MENUTYPE1, 'topmenu');
+        self::getMenus(MENUTYPE2, 'sidemenu');
+    }
+
+    private function getMenus($type, $name)
+    {
+        $cacheName = 'menu_'.$type.'_'.$name;
+        if(Cache::has($cacheName)) {
+            $menu = Cache::get($cacheName);
+        } else {
+            $menu = DB::table('menus')
+                ->where('type', $type)
+                ->where('status', ACTIVE)
+                ->orderByRaw(DB::raw("position = '0', position"))
+                ->orderBy('name')
+                ->get();
+            Cache::forever($cacheName, $menu);
+        }
+        view()->share($name, $menu);
     }
 
     /**
