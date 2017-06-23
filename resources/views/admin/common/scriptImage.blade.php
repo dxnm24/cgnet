a59f30e911ec11ba4ac3b832f6f643f480a3e492
Mad Raon <muadauyeu24@gmail.com>
2017-06-23 14:11:54 +0700

    fix

--- resources/views/admin/common/scriptImage.blade.php
+++ resources/views/admin/common/scriptImage.blade.php
@@ -25,7 +25,7 @@
 	             "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
 	             "save table contextmenu directionality emoticons template paste textcolor"
 	       ],
-	       content_css: "css/content.css",
+	       content_css: "/adminlte/plugins/tinymce/skins/lightgray/content.min.css",
 	       toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor | formatselect fontselect fontsizeselect | link unlink | removeformat",
 	       
 	       style_formats: [
