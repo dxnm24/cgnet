03930faddcb61cfc6a16d93d22b945f887478183
Mad Raon <muadauyeu24@gmail.com>
2017-06-26 16:17:20 +0700

    fix

--- resources/views/site/common/bottom.blade.php
+++ resources/views/site/common/bottom.blade.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="row column">
-		<p class="copy">Liên hệ quảng cáo: <a href="mailto:contact@choigames.net" target="_blank">contact@choigames.net</a></p>
+		<p class="copy">Liên hệ: <a href="mailto:admin@choigames.net" target="_blank">admin@choigames.net</a></p>
 		<p class="copy">© MMXVI <a href="/" target="_blank">choigames.net</a></p>
 		<p class="made-with-love">Made with ❤ in Hanoi</p>
 	</div>
